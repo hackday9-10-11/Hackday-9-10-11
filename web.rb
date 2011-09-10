1ec8cf1fbde8592e8b5230b219b8c494e2164084
Jeff Linwood <jlinwood@gmail.com>
2011-09-10 16:30:38 -0500

    fixed find

--- web.rb
+++ web.rb
@@ -7,23 +7,25 @@ set :public, File.dirname(__FILE__) + '/www'
 
 
 get '/found_trail_magic/:lat/:long/:contents' do
-uri = URI.parse(ENV['MONGOHQ_URL'])
-conn = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
-db = conn.db(uri.path.gsub(/^\//, ''))
-coll = db.collection("magic");
+	uri = URI.parse(ENV['MONGOHQ_URL'])
+	conn = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
+	db = conn.db(uri.path.gsub(/^\//, ''))
+	coll = db.collection("magic");
 
-  magic = Hash.new
-  magic["lat"] = params[:lat];
-  magic["long"] = params[:long];
-  magic["contents"] = params[:contents];
-  coll.insert magic
+	  magic = Hash.new
+	  magic["lat"] = params[:lat];
+	  magic["long"] = params[:long];
+	  magic["contents"] = params[:contents];
+	  coll.insert magic
 end
 
 get '/trail_magic.json' do
-uri = URI.parse(ENV['MONGOHQ_URL'])
-conn = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
-db = conn.db(uri.path.gsub(/^\//, ''))
-coll = db.collection("magic");
 	content_type :json
-	coll.find.to_json
+
+	uri = URI.parse(ENV['MONGOHQ_URL'])
+	conn = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
+	db = conn.db(uri.path.gsub(/^\//, ''))
+	coll = db.collection("magic");
+	
+	coll.find().to_json
 end
