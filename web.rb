48a740584f9cffd8a8bbb2b14908c936932aa172
Jeff Linwood <jlinwood@gmail.com>
2011-09-10 16:26:01 -0500

    Mongo support

--- web.rb
+++ web.rb
@@ -1,7 +1,29 @@
 require 'sinatra'
+require 'json'
+require 'mongo'
 
 set :public, File.dirname(__FILE__) + '/www'
 
-get '/app' do
-  
+
+
+get '/found_trail_magic/:lat/:long/:contents' do
+uri = URI.parse(ENV['MONGOHQ_URL'])
+conn = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
+db = conn.db(uri.path.gsub(/^\//, ''))
+coll = db.collection("magic");
+
+  magic = Hash.new
+  magic["lat"] = params[:lat];
+  magic["long"] = params[:long];
+  magic["contents"] = params[:contents];
+  coll.insert magic
+end
+
+get '/trail_magic.json' do
+uri = URI.parse(ENV['MONGOHQ_URL'])
+conn = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
+db = conn.db(uri.path.gsub(/^\//, ''))
+coll = db.collection("magic");
+	content_type :json
+	coll.find.to_json
 end
